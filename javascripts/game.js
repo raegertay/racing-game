b8999234d2f8116c5b1126f135890146779de76d
Raeger Tay <raegertay89@gmail.com>
2018-01-12 16:57:17 +0800

    Add win condition

--- javascripts/game.js
+++ javascripts/game.js
@@ -4,24 +4,43 @@ document.addEventListener('DOMContentLoaded', function() {
   const player = document.getElementById('player')
   const computer = document.getElementById('computer')
   const accelerateBtn = document.getElementById('accelerate-btn')
+  const playerSpeed = 5;
+  const computerSpeed = 5;
   let gameHasStarted = false
+  let intervalId = null;
 
   accelerateBtn.addEventListener('click', () => {
     if (!gameHasStarted) {
       gameHasStarted = true
       computerStart()
     }
-    let newLeftPercentage = Math.round(player.offsetLeft / racingTrack.offsetWidth * 100) + 1
+    let newLeftPercentage = Math.round(player.offsetLeft / racingTrack.offsetWidth * 100) + playerSpeed
     console.log(`Position of player: ${newLeftPercentage}%`)
     player.style.left = `${newLeftPercentage}%`
+    if ((player.offsetLeft + player.offsetWidth) > racingTrack.offsetWidth) {
+      const winMessage = document.createElement('p')
+      winMessage.append('You won!')
+      document.getElementsByTagName('body')[0].append(winMessage)
+      clearInterval(intervalId)
+      accelerateBtn.disabled = true
+    }
   })
 
   const computerStart = () => {
-    setInterval(() => {
-      let newLeftPercentage = Math.round(computer.offsetLeft / racingTrack.offsetWidth * 100) + 1
+    intervalId = setInterval(() => {
+      let newLeftPercentage = Math.round(computer.offsetLeft / racingTrack.offsetWidth * 100) + computerSpeed
       console.log(`Position of computer: ${newLeftPercentage}%`)
       computer.style.left = `${newLeftPercentage}%`
+      if ((computer.offsetLeft + computer.offsetWidth) > racingTrack.offsetWidth) {
+        const winMessage = document.createElement('p')
+        winMessage.append('You lost!')
+        document.getElementsByTagName('body')[0].append(winMessage)
+        clearInterval(intervalId)
+        accelerateBtn.disabled = true
+      }
     }, 200)
   }
+
+
 
 });
